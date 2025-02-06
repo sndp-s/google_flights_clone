f8baccb97b33215ba43b9a53743a1bf1bb8bcd1b
Sandeep Sharma <sandeeptech8@gmail.com>
2025-02-06 16:20:10 +0530

    feat: disable button when all of the required fields are not provided

--- src/components/SearchFlightsButton/SearchFlightsButtons.jsx
+++ src/components/SearchFlightsButton/SearchFlightsButtons.jsx
@@ -1,12 +1,30 @@
 import { Button, Typography } from "@mui/material";
 import { Search } from "@mui/icons-material";
+import useStore from "../../store";
 
 const SearchFlightsButton = ({ buttonProps, textProps }) => {
+  const originAirport = useStore((state) => state.originAirport);
+  const destinationAirport = useStore((state) => state.destinationAirport);
+  const journeyDate = useStore((state) => state.journeyDate);
+  const tripType = useStore((state) => state.tripType);
+  const cabinClass = useStore((state) => state.cabinClass);
+  const passengersCount = useStore((state) => state.passengersCount);
+
+  const disabled = (
+    !originAirport ||
+    !destinationAirport ||
+    !journeyDate ||
+    !tripType ||
+    !cabinClass ||
+    !passengersCount
+  );
+
   return (
     <Button
       variant='contained'
       startIcon={<Search />}
       {...buttonProps}
+      disabled={disabled}
     >
       <Typography variant='button' {...textProps}>
         Search
