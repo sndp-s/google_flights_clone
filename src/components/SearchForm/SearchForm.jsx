1090d07b971d764c1e80c0b5395a64b4c29127fb
Sandeep Sharma <sandeeptech8@gmail.com>
2025-02-06 23:26:04 +0530

    refactor: move container responsibility for search form to parent

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -8,86 +8,77 @@ import PassengerMenu from '../PassengerMenu/PassengerMenu';
 import CabinClassMenu from '../CabinClassMenu/CabinClassMenu';
 import SearchFlightsButton from '../SearchFlightsButton/SearchFlightsButtons';
 
+// TODO: Add filters
 function SearchForm() {
   return (
-    <Paper
-      sx={{
-        paddingX: 4,
-        paddingY: 2,
-        position: 'relative',
-        borderRadius: 2
-      }}
-      elevation={4}
+    <Grid
+      container
+      direction={'column'}
+      spacing={2}
     >
+
+      {/* forms */}
+      <Grid container>
+        <TripTypeSelector />
+        <PassengerMenu />
+        <CabinClassMenu />
+      </Grid>
+
       <Grid
         container
-        direction={'column'}
+        direction={{ sm: 'column', md: 'row' }}
         spacing={2}
       >
 
-        {/* forms */}
-        <Grid container>
-          <TripTypeSelector />
-          <PassengerMenu />
-          <CabinClassMenu />
-        </Grid>
-
-        <Grid
-          container
-          direction={{ sm: 'column', md: 'row' }}
-          spacing={2}
-        >
-
-          {/* origin-destination airport input */}
-          <Grid size={8} container spacing={0}>
-            <Grid size={5.5}>
-              <OriginAirportInput sx={{ width: '100%' }} />
-            </Grid>
-
-            <Grid
-              size={1}
-              container
-              justifyContent={'center'}
-              alignItems={'center'}
-            >
-              <IconButton>
-                <SwapHorizontalCircle fontSize={'large'} />
-              </IconButton>
-            </Grid>
-
-            <Grid size={5.5}>
-              <DestinationAirportInput sx={{ width: '100%' }} />
-            </Grid>
+        {/* origin-destination airport input */}
+        <Grid size={8} container spacing={0}>
+          <Grid size={5.5}>
+            <OriginAirportInput sx={{ width: '100%' }} />
           </Grid>
 
-          {/* flight date input */}
-          <Grid size={4} container>
-            <Grid size={12}>
-              <JourneyDatePicker sx={{ width: '100%' }} />
-            </Grid>
+          <Grid
+            size={1}
+            container
+            justifyContent={'center'}
+            alignItems={'center'}
+          >
+            <IconButton>
+              <SwapHorizontalCircle fontSize={'large'} />
+            </IconButton>
           </Grid>
 
+          <Grid size={5.5}>
+            <DestinationAirportInput sx={{ width: '100%' }} />
+          </Grid>
         </Grid>
 
-        {/* Search button */}
-        <Grid alignSelf={'center'}>
-          <SearchFlightsButton
-            buttonProps={{
-              sx: {
-                borderRadius: 8,
-                padding: '8px 16px',
-              },
-              size: 'large'
-            }}
-            textProps={{
-              sx: {
-                fontWeight: 'medium'
-              }
-            }}
-          />
+        {/* flight date input */}
+        <Grid size={4} container>
+          <Grid size={12}>
+            <JourneyDatePicker sx={{ width: '100%' }} />
+          </Grid>
         </Grid>
+
       </Grid>
-    </Paper>
+
+      {/* Search button */}
+      <Grid alignSelf={'center'}>
+        <SearchFlightsButton
+          buttonProps={{
+            sx: {
+              borderRadius: 8,
+              padding: '8px 16px',
+            },
+            size: 'large'
+          }}
+          textProps={{
+            sx: {
+              fontWeight: 'medium'
+            }
+          }}
+        />
+      </Grid>
+    </Grid>
   );
 }
 export default SearchForm;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,22 @@
-import SearchForm from "../../components/SearchForm/SearchForm";
+import { Paper } from '@mui/material';
+import SearchForm from '../../components/SearchForm/SearchForm';
 
 const HomePage = () => {
-  return (<SearchForm />)
+  return (
+    <Paper
+      sx={{
+        paddingX: 4,
+        paddingY: 2,
+        marginTop: 8,
+        borderRadius: 2,
+        width: '100%'
+      }}
+      elevation={4}
+    >
+      <SearchForm />
+    </Paper>
+
+  );
 };
 
 export default HomePage;
